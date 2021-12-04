60267d5444c93881fc49c3412f77732a55999c43
kuan <apkapb@gmail.com>
2021-12-04 10:03:03 +0800

    feat: v-model

---
+++ src/components/front/VModel.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup="setup">
+  const props = defineProps<{
+    modelValue: string;
+    age: number;
+  }>();
+  const emit = defineEmits(['update:age', 'update:modelValue']);
+  const handleValue = () => {
+    emit('update:modelValue', 'any.....');
+  };
+  const handleAge = () => {
+    emit('update:age', 33);
+  };
+</script>
+
+<template>
+  <div class="div">value: {{ props.modelValue }}</div>
+  <div><button class="border border-gray-200" @click="handleValue">change value</button></div>
+  <hr />
+  <div>age: {{ props.age }}</div>
+  <div>
+    <button @click="handleAge">Change Age</button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
